cdc9fb3102920b09122e1445b92742fe71227177
vishalvs4747-cyber <vishalvs4747@gmail.com>
2026-06-10 08:02:58 +0530

    angle of triangle file added

---
+++ angle_tri.py
@@ -0,0 +1,10 @@
+a = int(input("Enter angle A: "))
+b = int(input("Enter angle B: "))
+c = int(input("Enter angle C: "))
+
+angle = a + b + c
+
+if angle == 180:
+    print("The angles form a triangle.")    
+else:
+    print("The angles do not form a triangle." )
